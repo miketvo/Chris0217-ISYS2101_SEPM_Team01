925167fcbb39dc697b0dd85561cc871254490f0a
Chris0217 <102713143+Chris0217@users.noreply.github.com>
2023-05-18 23:32:51 +0700

    update (fetchController function name fixed)

--- server/controllers/fetchController.js
+++ server/controllers/fetchController.js
@@ -64,7 +64,7 @@ const getCaloriesController = async (req, res) => {
 };
 
 // This function gets the calories for the random label obtained from the previous function
-const getDietabelsController = async (req, res) => {
+const getDietLabelsController = async (req, res) => {
   console.log("getDietLabelsController executed!");
 
   // call getLabelController to fetch a random label
@@ -96,10 +96,47 @@ const getDietabelsController = async (req, res) => {
   });
 };
 
+
+// This function fetches all ingredients from the database
+const getAllIngredientsController = async (req, res) => {
+  console.log("getAllIngredientsController executed!");
+  // define the SQL query to fetch all ingredients
+  const query = "SELECT ingredients FROM data;";
+  // execute the query using the db object and handle the results
+  db.query(query, function (err, data) {
+    // check if there's an error
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
+    // check if there's data and it has length
+    if (data && data.length > 0) { // Check if data is not undefined and has length
+      const allIngredients = []; // initialize an empty array to store unique ingredients
+      // loop through each row of data
+      data.forEach((row) => {
+        const ingredients = JSON.parse(row.ingredients); // convert ingredient to lowercase to avoid duplicates
+        // loop through each ingredient in the array
+        ingredients.forEach((ingredient) => {
+          const lowerCaseIngredient = ingredient.toLowerCase(); // convert ingredient to lowercase to avoid duplicates
+          // check if the lower case ingredient is not already in the allIngredients array
+          if (!allIngredients.includes(lowerCaseIngredient)) {
+            allIngredients.push(lowerCaseIngredient); // if not, add it to the allIngredients array
+          }
+        });
+      });
+      return res.json(allIngredients); // return the array of unique ingredients as JSON
+    } else {
+      // if there's no data, return an empty array as JSON
+      return res.json([]);
+    }
+  });
+};
+
 module.exports = {
   getAllController,
   getLabelController,
   getCaloriesController,
-  getDietabelsController,
+  getDietLabelsController,
+  getAllIngredientsController
 };
 
