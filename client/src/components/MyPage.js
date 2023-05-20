e896bd7008edfc53f5ff741ea0a903ba5ff0b42c
Chris0217 <102713143+Chris0217@users.noreply.github.com>
2023-05-20 22:18:36 +0700

    My page update

--- client/src/components/MyPage.js
+++ client/src/components/MyPage.js
@@ -11,7 +11,12 @@ function MyPage() {
     const [sex, setSex] = useState('');
     const [height, setHeight] = useState('');
     const [weight, setWeight] = useState('');
+    const [allergen, setAllergen] = useState('')
+    const [unpIngredients, setUnpIngredients] = useState('')
   
+    const [editMode, setEditMode] = useState(false);
+
+
     useEffect(() => {
       const fetchUserInfo = async () => {
         try {
@@ -22,11 +27,6 @@ function MyPage() {
             setUserData(userData);
 
             if(userData) {
-                setAge(userData[0].age);
-                setSex(userData[0].sex);
-                setHeight(userData[0].height);
-                setWeight(userData[0].weight);
-      
                 console.log('--------------------------');
                 console.log('username:', username);
                 console.log('User Data:', userData);
@@ -34,7 +34,16 @@ function MyPage() {
                 console.log("sex:", userData[0].sex);
                 console.log("height:", userData[0].height);
                 console.log("weight:", userData[0].weight);
+                console.log("allergen:", userData[0].allergen);
+                console.log("unpIngredients:", userData[0].unpreferred_ingredients);
                 console.log('--------------------------');
+                
+                setAge(userData[0].age);
+                setSex(userData[0].sex);
+                setHeight(userData[0].height);
+                setWeight(userData[0].weight);
+                setAllergen(userData[0].allergen);
+                setUnpIngredients(userData[0].unpreferred_ingredients)
             }
 
         } catch (error) {
@@ -45,6 +54,20 @@ function MyPage() {
       fetchUserInfo();
     }, []);
 
+    const handleMode = async (e) => {
+      e.preventDefault();
+      if (editMode) {
+        setAge(userData[0].age);
+        setSex(userData[0].sex);
+        setHeight(userData[0].height);
+        setWeight(userData[0].weight);
+        setAllergen(userData[0].allergen);
+        setUnpIngredients(userData[0].unpreferred_ingredients)
+        setEditMode(false);
+      } else {
+        setEditMode(true)
+      }
+    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -57,10 +80,11 @@ function MyPage() {
                 headers: { 'Content-Type': 'application/json' },
                 }
             );
-            // 업데이트 성공 시 필요한 작업 수행
-        
+            alert("Your information is updated!");
+            setEditMode(false)
         } catch (err) {
-            // 업데이트 실패 시 필요한 작업 수행
+            alert("Update failed to proceed.")
+            handleMode();
         }
     };
 
@@ -73,7 +97,7 @@ function MyPage() {
             <h2>General Information</h2>
             <div class="next">
               <img
-                src={require("../images/user_picture.jpg")}
+                src={require("../images/user_picture.png")}
                 className="userPic"
                 alt="UserPic"
               />
@@ -88,65 +112,134 @@ function MyPage() {
                 />
                 <label htmlFor="email">Age</label>
                 <input
-                  type="integer"
+                  type="number"
                   id="age"
                   name="age"
                   value={age}
                   onChange={(e)=>setAge(e.target.value)}
-
+                  readOnly={editMode ? false: true}
                 />
                 <label htmlFor="sex">Sex</label>
-                <select name="sex" id="sex" value={sex} onChange={(e)=>setSex(e.target.value)}>
-                  <option value="choose">Choose</option>
-                  <option value="female">Female</option>
-                  <option value="male">Male</option>
-                </select>
+                
+                {editMode 
+                  ? 
+                  <select name="sex" id="sex" value={sex} onChange={(e)=>setSex(e.target.value)}>
+                    <option selected hidden>Select to change</option>
+                    <option value="F">Female</option>
+                    <option value="M">Male</option>
+                  </select> 
+                  : 
+                  <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={sex}
+                  readOnly
+                />
+                }
               </div>
             </div>
-            <label htmlFor="height">Height</label>
-            <input
-              type="float"
-              id="height"
-              name="height"
-              value={height}
-              onChange={(e)=>setHeight(e.target.value)}
-
-            />
-            <label htmlFor="weight">Weight</label>
-            <input
-              type="float"
-              id="weight"
-              name="weight"
-              value={weight}
-              onChange={(e)=>setWeight(e.target.value)}
-
-            />
+            <br></br>
+            <div class="forms">
+              <div class="hw">
+                <div>
+                  <label htmlFor="height">Height (cm)</label>
+                  <input
+                    type="number"
+                    step="0.01"
+                    id="height"
+                    name="height"
+                    value={height}
+                    onChange={(e)=>setHeight(e.target.value)}
+                    readOnly={editMode ? false: true}
+                  />
+                </div>
+                <br></br>
+                <div>
+                  <label htmlFor="weight">Weight (kg)</label>
+                  <input
+                    type="number"
+                    step="0.01"
+                    id="weight"
+                    name="weight"
+                    value={weight}
+                    onChange={(e)=>setWeight(e.target.value)}
+                    readOnly={editMode ? false: true}
+                  />
+                </div>
+              </div>
+              <div class="divBmi">
+                <div>
+                  <label htmlFor="bmi">BMI</label>
+                  <input
+                    type="number"
+                    step="0.01"
+                    id="bmi"
+                    name="bmi"
+                    value={(weight/((height/100)^2)).toFixed(2)}
+                    onChange={(e)=>setWeight(e.target.value)}                  
+                    readOnly
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="divBmiImg">
+              <img
+                  src={require("../images/BMIscale.png")}
+                  className="bmi"
+                  alt="BMI_scale"
+                />
+            </div>
           </div>
           <div class="condition">
             <h2>Condition</h2>
             <label for="allergen">Allergens</label>
-            <select name="allergen" id="allergen">
-              <option value="choose">Choose</option>
-              <option value="apple">Apple-free</option>
-              <option value="cranberry">Cranberry-free</option>
-              <option value="peach">Peach-free</option>
-              <option value="peanut">Peanut-free</option>
-              <option value="raison">Raison-free</option>
-            </select>
+            <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={allergen}
+                  readOnly
+                />
+            {editMode 
+              ? 
+              <select name="allergen" id="allergen" /*onChange={(e)=>setAllergens(e.target.value)*/>
+                <option selected hidden >Select to change</option>
+                <option value="apple">Apple-free</option>
+                <option value="cranberry">Cranberry-free</option>
+                <option value="peach">Peach-free</option>
+                <option value="peanut">Peanut-free</option>
+                <option value="raison">Raison-free</option>
+              </select>
+              : 
+              false
+            }
             <label for="unpreferred">Unpreferred Ingredients</label>
-            <select name="unpreferred" id="unpreferred">
-              <option value="choose">Choose</option>
-              <option value="apple">Apple-free</option>
-              <option value="cranberry">Cranberry-free</option>
-              <option value="peach">Peach-free</option>
-              <option value="peanut">Peanut-free</option>
-              <option value="raison">Raison-free</option>
-            </select>
+            <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={unpIngredients}
+                  readOnly
+                />
+            {editMode 
+              ? 
+              <select name="unpreferred" id="unpreferred" /*onChange={(e)=>setIngredients(e.target.value)*/>
+                <option selected hidden>Select to change</option>
+                <option value="apple">Apple-free</option>
+                <option value="cranberry">Cranberry-free</option>
+                <option value="peach">Peach-free</option>
+                <option value="peanut">Peanut-free</option>
+                <option value="raison">Raison-free</option>
+              </select>
+              : 
+              false
+            }
           </div>
         </div>
         <div class="buttons" >
-
-          <button type="submit">Save</button>
+          <button type="changeMode" onClick={handleMode}>{editMode ? 'Cancel' : 'Edit'}</button>
+          <button type="submit" disabled={editMode ? false: true}>Save</button>
         </div>
       </form>
     </section>
--- server/controllers/mypageController.js
+++ server/controllers/mypageController.js
@@ -26,43 +26,35 @@ function getUserInfo(req, res) {
       };
 
 const handleEdit= async (req, res) => {
-    // console.log("테스트", req.session.username);
-    // const age = req.body.age;
-    // const sex = req.body.sex;
-    // const height = req.body.height;
-    // const weight = req.body.weight;
+    const cachedUsername = sessionUtil.getUsernameFromSession(req);
+    const age = req.body.age;
+    const sex = req.body.sex;
+    const height = req.body.height;
+    const weight = req.body.weight;
 
-    // //console.log(sessionStorage.getItem("name"));
-    // var sql1 = "SELECT * FROM users WHERE name = '"+req.session.name+"';";
-    // db.query(sql1, function(err, rows) {
-    //     if(rows.length == 1) {
-    //         var sql2 = {
-    //             age: age,
-    //             sex: sex,
-    //             height: height,
-    //             weight: weight,
-    //         }; 
-    //     var sql3 = db.query("UPDATE users SET ? WHERE name = '"+req.session.name+"'", sql2, function(err, rows) {
-    //         if(err) {
-    //             return console.log("errorcode:", err);
-    //         } else {
-    //             console.log("input age:[", age, "], sex: [", sex, "], input height: [", height, "], input weight: [", weight, "]");
-
-    //             req.session.age = age;
-    //             req.session.height = height;
-    //             req.session.weight = weight;
-    //             req.session.save();
-
-    //             console.log(req.session);
-                
-    //             res.end();
-    //         }
-    //     })
-    // } else {
-    //     console.log("errorcode:", err);
-    //     return res.sendStatus(400);
-    // }
-// });
+    var sql1 = "SELECT * FROM users WHERE name = '"+cachedUsername+"';";
+    db.query(sql1, function(err, rows) {
+        if(rows.length == 1) {
+            var newSetting = {
+                age: age,
+                sex: sex,
+                height: height,
+                weight: weight
+            }; 
+            var sql2 = "UPDATE users SET ? WHERE name = '"+cachedUsername+"'"
+            db.query(sql2, newSetting, function(err, rows) {
+                if(err) {
+                    return console.log("errorcode:", err);
+                } else {
+                    console.log("input age:[", age, "], sex: [", sex, "], input height: [", height, "], input weight: [", weight, "]");
+                    res.end();
+                }
+            })
+        } else {
+            console.log("errorcode:", err);
+            return res.sendStatus(400);
+        }
+    });
 };
 
 
